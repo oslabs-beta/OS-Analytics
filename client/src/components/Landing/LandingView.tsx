361dc3814f8f401cf064b5deebacc89021d0f0f7
Eric DiMarzio <127116933+EricDiMarzio@users.noreply.github.com>
2024-08-27 15:25:50 -0500

    small css changes

--- client/src/components/Landing/LandingView.tsx
+++ client/src/components/Landing/LandingView.tsx
@@ -12,7 +12,6 @@ export default function Landing() {
         <NavMobile />
         <Hero />
         <Footer />
-        <Loading />
         </div>
   )
 }
--- client/src/state/Atoms.tsx
+++ client/src/state/Atoms.tsx
@@ -11,9 +11,12 @@ export const loadingAtom = atom<boolean>(false);
 // User's Click Data
 export const userDataAtom = atom<[]>([]);
 export const websitesAtom = atom<[]>([])
+//manipulated by the dropdown choice in Dashboard.tsx
 export const activeWebsiteAtom = atom<string>('');
 export const websiteDataAtom = atom((get) => {
+  //references the active website atom
   const activeWebsite = get(activeWebsiteAtom);
+  //references the complete data set and filters according to the active website atom
   return get(userDataAtom).filter((el:any) => el.website_name === activeWebsite);
 });
 export const timeFrameAtom = atom<string>('24hours');
