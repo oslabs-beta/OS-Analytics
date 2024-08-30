1deb1f12895da5ece468eb3a344aa35b86580202
David Naymon <naymondavid@gmail.com>
2024-08-30 11:32:14 -0400

    api key/ai/oauth features

--- client/src/components/ChartPages/AllUserData.tsx
+++ client/src/components/ChartPages/AllUserData.tsx
@@ -5,10 +5,26 @@ import AiResponseComponent from "./Charts/aiResponse";
 import { userDataAtom } from "../../state/Atoms";
 import { useAtom } from "jotai";
 import styles from "./Charts.module.css";
-import { mapUserData } from '../../services/extractData';
+import { mapUserData } from "../../services/extractData";
+
+//to add graph in other components
+
+/*
+*Imports from jotai to get global state and map hook*
+import { userDataAtom,websiteDataAtom } from "../../state/Atoms"; <------ grab global state hooks
+import { useAtom } from "jotai"; <------ needed import to use global state as variables
+import { mapUserData } from '../../services/extractData';  <------ map hook to parse the extracted global state data
+
+
+  const [userData] = useAtom(userDataAtom);<------ grabing the gobal state data from the hook
+  const mappedData = mapUserData(userData) <------ parsing it before ending it to graphs algorith is found in /services/extractData
+
+ <BarGraph data={mappedData} keyword={"website"} /><---- using the graph anywhere and giving it data and a keyword on which data to display (clicks per "website")
+
+*/
 const ClickDataVisualization = () => {
-  const [userData] = useAtom(userDataAtom); 
-  const mappedData = mapUserData(userData)
+  const [userData] = useAtom(userDataAtom);
+  const mappedData = mapUserData(userData);
 
   return (
     <div className={styles.chartDisplay}>
