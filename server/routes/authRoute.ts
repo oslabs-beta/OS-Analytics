13ac708e71dfb1f1b8df622824fecb93df31c268
Peter Larcheveque <larchevequepeter@gmail.com>
2024-08-30 17:51:24 -0700

    edited vite config file and added backend url to make developement build easier

--- server/routes/authRoute.ts
+++ server/routes/authRoute.ts
@@ -10,7 +10,7 @@ router.get('/oauth',
     passport.authenticate('google', { failureRedirect: 'http://localhost:3000/login', session: false }),
     (req, res) => {
       const { token, user } = req.user as User;
-      res.redirect(`http://activity-tracker-s3.s3-website-us-west-1.amazonaws.com/login?token=${token}&email=${user.email}`);
+      res.redirect(`http://localhost:3000/login?token=${token}&email=${user.email}`);
     }
   );
 
