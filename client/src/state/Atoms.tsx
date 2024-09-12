bfdbb35c77af3f4dfa6221bcd724cdb3ecec3d43
David Naymon <naymondavid@gmail.com>
2024-09-12 13:38:52 -0400

    working server

--- client/src/state/Atoms.tsx
+++ client/src/state/Atoms.tsx
@@ -2,8 +2,10 @@ import { atom } from "jotai";
 import { QueryData, referralData } from "../../types";
 
 // const userStore = createStore();
+// IF RUNNING ON LOCAL HOST, REPLACE backendURL value with empty string.
+//http://ec2-18-144-89-57.us-west-1.compute.amazonaws.com
 
-export const backendUrl: string = "http://ec2-18-144-89-57.us-west-1.compute.amazonaws.com"; // backend URI
+export const backendUrl: string = "http://ec2-18-144-89-57.us-west-1.compute.amazonaws.com:8080"; // backend URI
 export const activeUserAtom = atom<string>("");
 export const activeNavAtom = atom<boolean>(false);
 
